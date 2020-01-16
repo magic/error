973448dee3aee2f97dbe488f749d15cd28435579
Jascha <jascha@jaeh.at>
2020-01-16 03:03:26 +0100

    accepts errors as arguments

--- src/index.mjs
+++ src/index.mjs
@@ -1,14 +1,21 @@
-export const error = (msg, name = 'E_UNKNOWN') => {
-  const err = new Error(msg)
-  err.name = name
+export const error = (err, name = 'E_UNKNOWN') => {
+  if (err instanceof Error) {
+    name = err.code || err.name
+    err.code = name
+    err.name = name
+  } else {
+    err = new Error(err)
+    err.name = name
+    err.code = name
+  }
 
   // clean stack
-  // remove name, message, and first line of stack (which is this file)
+  // remove name, message, and !first and second! line of stack (if they include this file)
   err.stack = err.stack
-    .replace(err.name, '')
-    .replace(error.message, '')
+    .replace(`${err.name}:`, '')
+    .replace(`${err.code}:`, '')
     .split('\n')
-    .filter((_, i) => i !== 1)
+    .filter((c, i) => (i <= 1 ? c.includes('/error/src/index.mjs') : false))
     .join('\n')
 
   return err
