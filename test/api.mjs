e8f2b6ae3174a785b1dbca5f39ffdbfce5ac09a8
Jascha <jascha@jaeh.at>
2020-01-16 03:03:33 +0100

    add tests

--- test/api.mjs
+++ test/api.mjs
@@ -1,11 +1,26 @@
-import { is } from '@magic/test'
+import { is, fs, tryCatch } from '@magic/test'
 
-import err from '../src/index.mjs'
+import error from '../src/index.mjs'
 
 const testError = new Error('message')
 
 export default [
-  { fn: err('message', 'E_CUSTOM').name, expect: 'E_CUSTOM', info: 'error.name can be set' },
-  { fn: err('message').message, expect: 'message', info: 'error.message can be set' },
-  { fn: err('message').name, expect: 'E_UNKNOWN', info: 'default name is E_UNKNOWN' },
+  { fn: error('message', 'E_CUSTOM').name, expect: 'E_CUSTOM', info: 'error.name can be set' },
+  { fn: error('message').message, expect: 'message', info: 'error.message can be set' },
+  { fn: error('message').name, expect: 'E_UNKNOWN', info: 'default name is E_UNKNOWN' },
+  { fn: error('message').code, expect: 'E_UNKNOWN', info: 'default name is E_UNKNOWN' },
+
+  { fn: error(new Error('message')).name, expect: 'Error', info: 'error default name is Error' },
+  { fn: error(new Error('message')).code, expect: 'Error', info: 'error default name is Error' },
+  {
+    fn: error(new Error('message')).message,
+    expect: 'message',
+    info: 'error.message is set if error is passed',
+  },
+
+  {
+    fn: tryCatch(fs.mkdirp, ''),
+    expect: t => t.name === 'E_ARG_EMPTY',
+    info: 'fs.mkdirp error gets returned as expected',
+  },
 ]
