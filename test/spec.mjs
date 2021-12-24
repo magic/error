14535212150494fbb6b5f1dc4c0138bbc0340a26
jascha <j@jaeh.at>
2021-12-24 16:10:55 +0100

    test/spec: remove unused var

--- test/spec.mjs
+++ test/spec.mjs
@@ -2,8 +2,6 @@ import { is } from '@magic/test'
 
 import err, { error } from '../src/index.mjs'
 
-const testError = new Error('message')
-
 export default [
   { fn: () => err, expect: is.function, info: 'error is a function' },
   { fn: () => err('message', 'name'), expect: is.error, info: 'error returns an Error' },
